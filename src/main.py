6edbd58d16a057eba8223b2b9833d25a4345ff8f
Danil Vinogradov <danila6464@gmail.com>
2025-03-22 20:10:39 +0300

    create check combs for 5 dices and print possible scores and create game board for few players

--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
 import random
+# import pandas as pd
+# import numpy as np
+import sys
 
 class ArraySizeError(Exception):
     "Invalid array size"
@@ -41,10 +44,8 @@ def print_five_dice(result):
     for i in range(len(faces)):
         for j in range(len(faces[0])):
             print(faces[j][i], end=' ')
-        if i != len(faces) - 1:
-            print()
+        print()
 
-    
 
 def roll_five_dice() -> list[int]:
     results = []
@@ -62,7 +63,8 @@ def reroll_few_dice(result: list[int], reroll_dices: list[int]):
 def one_turn():
     print("Rolling five dice...")
     result = roll_five_dice()
-    print(f"Result: {result}")
+    # print(f"Result: {result}")
+    check_all_combinations(result)
     print_five_dice(result)
     for i in range(2):
         print(f"{i + 1}: Rerolling...")
@@ -71,6 +73,7 @@ def one_turn():
             validate_array(reroll_dices)
             reroll_few_dice(result, reroll_dices)
             print(f"Updated result: {result}")
+            check_all_combinations(result)
             print_five_dice(result) 
         except ArraySizeError as e:
             print(f"Size Error: {e}")
@@ -81,25 +84,191 @@ def one_turn():
         except Exception as e:
             print(f"Unexpected Error: {e}")
 
-def main():
-    # one_turn()
-    result = roll_five_dice()
-    print(f"Result: {result}")
-    print_five_dice(result)
-    # print(f"Results: {results}")
-    # for die in results:
-    #     face = create_object_dice(die)
-    #     print_dice(face)
-    # print("\nRerolling a few dice... 1 and 4")
-    # # try:
-    # reroll_dices = list(map(int, input().split(" ")))
-    # # except
 
-    # reroll_few_dice(results, reroll_dices)
-    # print(f"Updated results: {results}")
-    # for die in results:
-    #     die_object = die
-    #     die_object.print_dice()
+def add_players():
+    players = []
+    count_players = int(input("Количество игроков: "))
+    if count_players < 1 or count_players > 4:
+        print("Количество игроков должно быть между 1 и 4.")
+        return
+    for i in range(count_players):
+        player_name = input(f"Имя игрока {i + 1}: ")
+        players.append(player_name)
+    return players
+
+
+def create_start_table(combinations_ru, players):
+    count_players = len(players)
+    for comb, player in combinations_ru.items():
+        for i in range(count_players):
+            if comb == "64/35":
+                player.append(players[i])
+            else:
+                player.append("")
+
+
+def draw_table(combinations_ru, players):
+    count_players = len(players)
+    max_name = max(len(name) for name in players)
+    sys.stdout.write("\033[F" * (count_players - 25))
+    for comb, player in combinations_ru.items():
+        print("+" + "-" * 17 + ("+" + "-" * (max_name + 4)) * count_players + "+")
+        print(f"| {comb:^15} |", end="")
+        for i in range(count_players):
+            print(f" {player[i]:^{max_name + 2}} |",end="")
+        print()
+
+
+def two_pairs_check(result):
+    counts = {}
+    s = 0
+    for num in result:
+        if num not in counts:
+            counts[num] = 1
+        else:
+            counts[num] += 1
+    temp = -1
+    for num, count in counts.items():
+        if count >= 2 and temp == -1:
+            temp = num
+        elif count >= 2 and temp != 1:
+            s = temp * 2 + num * 2
+    return s
+            
+
+def full_house_check(result):
+    counts = {}
+    for num in result:
+        if num not in counts:
+            counts[num] = 1
+        else:
+            counts[num] += 1
+    s = 0
+    if len(counts) == 2:
+        for num, count in counts.items():
+            if count == 2 or count == 3:
+                s += num * count
+    return s
+        
+    
+def small_straight_check(result):
+    counts = {}
+    temp = result.copy()
+    temp.sort()
+    n = len(temp)
+    res = 0
+    for i in range(n - 3):
+        sub = temp[i:i+4]
+        valid = True
+        for j in range(3):
+            if sub[j + 1] != sub[j] + 1:
+                valid = False
+                break
+        if valid:
+            s = sum(sub)
+            if s > res:
+                res = s
+    return res
+
+
+def large_straight_check(result):
+    counts = {}
+    temp = result.copy()
+    temp.sort()
+    n = len(temp)
+    res = 0
+    for i in range(n - 4):
+        sub = temp[i:i+5]
+        valid = True
+        for j in range(4):
+            if sub[j + 1] != sub[j] + 1:
+                valid = False
+                break
+        if valid:
+            s = sum(sub)
+            if s > res:
+                res = s
+    return res
+
+def chance_check(dices):
+    return sum(dices)
+
+def number_check(dices, number):
+    number_in_dices = dices.count(number)
+    return number_in_dices * number
+
+def number_of_a_kind_check(dices, number):
+    counts = {}
+    for num in dices:
+        if num not in counts:
+            counts[num] = 1
+        else:
+            counts[num] += 1
+    res = 0
+    for num, count in counts.items():
+        if count >= number:
+            s = number * num
+            if s > res: res = s
+    return res
+
+
+def check_all_combinations(result):
+    res = {
+        "1": number_check(result, 1),
+        "2": number_check(result, 2),
+        "3": number_check(result, 3),
+        "4": number_check(result, 4),
+        "5": number_check(result, 5),
+        "6": number_check(result, 6),
+        "Пара": number_of_a_kind_check(result, 2),
+        "Две пары": two_pairs_check(result),
+        "Сет": number_of_a_kind_check(result, 3),
+        "Каре": number_of_a_kind_check(result, 4),
+        "Фулл Хаус": full_house_check(result),
+        "Малый стрит": small_straight_check(result),
+        "Большой стрит": large_straight_check(result),
+        "Ецци": number_of_a_kind_check(result, 5),
+        "Шанс": chance_check(result)
+    }
+    for comb, r in res.items():
+        if r == 0:
+            continue
+        else:
+            print(comb, r)
+def main():
+    combinations_ru = {
+        "64/35": [],
+        "1": [],
+        "2": [],
+        "3": [],
+        "4": [],
+        "5": [],
+        "6": [],
+        "Пара": [],
+        "2 пары": [],
+        "Сет": [],
+        "Каре": [],
+        "Фулл хаус": [],
+        "Малый стрит": [],
+        "Большой стрит": [],
+        "Ецци": [],
+        "Шанс": [],
+        "Сумма": []
+        }
+    print("Welcome to Yezzi!")
+    players = add_players()
+    count_players = len(players)
+    create_start_table(combinations_ru, players)
+    print("Starting grid:")
+    draw_table(combinations_ru, players)
+    turn = 1
+    while True:
+        print(f"Turn {turn}")
+        for i in range(len(players)):
+            print(f"{players[i]}:")
+            one_turn()
+        turn += 1
+
 
 
 
