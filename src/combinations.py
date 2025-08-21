194035068e99f6edb509deb403ee58559272906c
FenixPam <danila6464@gmail.com>
2025-08-21 14:49:58 +0300

    Отдельная функция для посдсчета уникальных костей

--- src/combinations.py
+++ src/combinations.py
@@ -3,6 +3,16 @@ Module for checking various combinations in the Yatzy game.
 Contains functions for calculating points based on rolled dice.
 Each function takes a list of dice values and returns the number of points.
 """
+def counts_dices(dices: list[int]) -> dict:
+    """Подсчитывает частоты значений костей и возвращает словарь вида {значение: количество}."""
+    counts = {}
+    for num in dices:
+        if not num in counts:
+            counts[num] = 1
+        else:
+            counts[num] += 1
+    return counts
+
 
 def number_check(dices: list[int], number: int) -> int:
     """Calculates the sum of points for a specific number 
@@ -14,12 +24,7 @@ def number_check(dices: list[int], number: int) -> int:
 def number_of_a_kind_check(dices: list[int], number: int) -> int:
     """Checks for a combination of the specified number of 
     identical numbers and returns their sum."""
-    counts = {}
-    for num in dices:
-        if num not in counts:
-            counts[num] = 1
-        else:
-            counts[num] += 1
+    counts = counts_dices(dices)
     res = 0
     for num, count in counts.items():
         if count >= number:
@@ -27,39 +32,29 @@ def number_of_a_kind_check(dices: list[int], number: int) -> int:
             res = max(res, s)
     return res
 
-def two_pairs_check(dice: list[int]) -> int:
+def two_pairs_check(dices: list[int]) -> int:
     """Checks for two pairs of identical numbers and returns the sum of these pairs."""
-    counts = {}
-    s = 0
-    for num in dice:
-        if num not in counts:
-            counts[num] = 1
-        else:
-            counts[num] += 1
+    counts = counts_dices(dices)
+    res = 0
     temp = -1
     for num, count in counts.items():
         if count >= 2 and temp == -1:
             temp = num
         elif count >= 2 and temp != -1:
             s = temp * 2 + num * 2
-    return s
+    return res
 
 
-def full_house_check(dice: list[int]) -> int:
+def full_house_check(dices: list[int]) -> int:
     """Checks for a full house combination (three of a kind plus a pair) 
     and returns the sum of all dice."""
-    counts = {}
-    for num in dice:
-        if num not in counts:
-            counts[num] = 1
-        else:
-            counts[num] += 1
-    s = 0
+    counts = counts_dices(dices)
+    res = 0
     if len(counts) == 2:
         for num, count in counts.items():
             if count in (2, 3):
                 s += num * count
-    return s
+    return res
 
 
 def small_straight_check(dice: list[int]) -> int:
